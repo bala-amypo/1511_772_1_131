5082ea38054ef68668e08eaef2d97bb0b4443516
bala-amypo <bala@amypo.in>
2025-12-21 08:16:52 +0000

    User commit

--- src/main/java/com/example/demo/repository/ZoneRepository.java
+++ src/main/java/com/example/demo/repository/ZoneRepository.java
@@ -3,12 +3,12 @@ package com.example.demo.repository;
 import com.example.demo.entity.Zone;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
+import java.util.List;
 
 public interface ZoneRepository extends JpaRepository<Zone, Long> {
 
-    Optional<Zone> findByZoneName(String name);
+    Optional<Zone> findByZoneName(String zoneName);
 
     List<Zone> findByActiveTrueOrderByPriorityLevelAsc();
 }
