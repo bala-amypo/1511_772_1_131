dcb0e39666412641d1c6e19a1171134a70450860
bala-amypo <bala@amypo.in>
2025-12-21 16:51:25 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/ZoneServiceImpl.java
@@ -0,0 +1,50 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.entity.Zone;
+import com.example.demo.repository.ZoneRepository;
+import com.example.demo.service.ZoneService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ZoneServiceImpl implements ZoneService {
+
+    private final ZoneRepository zoneRepository;
+
+    // Constructor Injection (MANDATORY)
+    public ZoneServiceImpl(ZoneRepository zoneRepository) {
+        this.zoneRepository = zoneRepository;
+    }
+
+    @Override
+    public Zone createZone(Zone zone) {
+        return zoneRepository.save(zone);
+    }
+
+    @Override
+    public Zone updateZone(Long id, Zone zone) {
+        Zone existing = getZoneById(id);
+        existing.setZoneName(zone.getZoneName());
+        existing.setPriorityLevel(zone.getPriorityLevel());
+        existing.setPopulation(zone.getPopulation());
+        return zoneRepository.save(existing);
+    }
+
+    @Override
+    public Zone getZoneById(Long id) {
+        return zoneRepository.findById(id).orElseThrow();
+    }
+
+    @Override
+    public List<Zone> getAllZones() {
+        return zoneRepository.findAll();
+    }
+
+    @Override
+    public void deactivateZone(Long id) {
+        Zone zone = getZoneById(id);
+        zone.setActive(false);
+        zoneRepository.save(zone);
+    }
+}
