febcd44585a5eb20e53d2d745298fa38e85e60b5
bala-amypo <bala@amypo.in>
2025-12-26 04:33:17 +0000

    User commit

---
+++ src/main/java/com/example/demo/dto/ZoneDTO.java
@@ -0,0 +1,52 @@
+package com.example.demo.dto;
+
+public class ZoneDTO {
+
+    private Long id;
+    private String zoneName;
+    private int priorityLevel;
+    private int population;
+    private boolean active;
+
+    public ZoneDTO() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getZoneName() {
+        return zoneName;
+    }
+
+    public void setZoneName(String zoneName) {
+        this.zoneName = zoneName;
+    }
+
+    public int getPriorityLevel() {
+        return priorityLevel;
+    }
+
+    public void setPriorityLevel(int priorityLevel) {
+        this.priorityLevel = priorityLevel;
+    }
+
+    public int getPopulation() {
+        return population;
+    }
+
+    public void setPopulation(int population) {
+        this.population = population;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+}
